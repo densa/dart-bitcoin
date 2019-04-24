ca916fc20ef8cc00427a4d5fd31b76cb0c304bcb
Noah <noah@noahseidman.com>
2019-04-24 18:10:03 -0400

    Add withCheckum name param.

--- lib/src/wire/message.dart
+++ lib/src/wire/message.dart
@@ -85,7 +85,7 @@ abstract class Message extends BitcoinSerializable {
   }
 
   /// Encode a message to serialized format.
-  static Uint8List encode(Message msg, String magicValue, int pver) {
+  static Uint8List encode(Message msg, String magicValue, int pver, {bool withChecksum = true}) {
     var buffer = new bytes.Buffer();
 
     // write magic value and command
@@ -98,7 +98,9 @@ abstract class Message extends BitcoinSerializable {
 
     // write payload size and checksum
     writeUintLE(buffer, payloadBuffer.length);
-    buffer.add(payloadBuffer.checksum().sublist(0, 4));
+    if (withChecksum) {
+      buffer.add(payloadBuffer.checksum().sublist(0, 4));
+    }
 
     // write the actual payload
     writeBytes(buffer, payloadBuffer.asBytes());
